2ad9b3ddad60f62fa65ed28be2742daa35503dfc
Paulo Khouri <paulokhouri@gmail.com>
2013-04-17 10:37:18 -0300

    Initial Commit

---
+++ MagicalRecordSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MagicalRecordSample
+//
+//  Created by Paulo Khouri on 4/17/13.
+//  Copyright (c) 2013 Mobilitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MagicalRecordSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MagicalRecordSample
+//
+//  Created by Paulo Khouri on 4/17/13.
+//  Copyright (c) 2013 Mobilitate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
